09ac9447e0eb53b191adf52c03d758d061f3a51c
KamSaf <bopaf42@gmail.com>
2024-03-03 15:14:40 +0100

    Created backend and frontend for Viginere functions

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from utils.caesar import CaesarCipher as Caesar
+from utils.vigenere import VigenereCipher as Vigenere
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -24,26 +26,28 @@ async def caesar(request: Request):
 async def caesar_encode(request: Request, text: str = Form(..., description='text'), shift: int = Form(..., description='shift')):
     encoded_text = Caesar.cipher(text=text, shift=shift)
     if encoded_text:
-        return templates.TemplateResponse(request=request, name="caesar.html", context={'input_text_encode': text, 'encoded_text': encoded_text, 'shift_encode': shift, 'error': False})
+        return templates.TemplateResponse(request=request, name="caesar.html", context={'input_text_encode': text, 'encoded_text': encoded_text, 'shift_encode': shift, 'error_encode': False})
     else:
-        return templates.TemplateResponse(request=request, name="caesar.html", context={'input_text_encode': text, 'shift_encode': shift, 'error': True})
+        return templates.TemplateResponse(request=request, name="caesar.html", context={'input_text_encode': text, 'shift_encode': shift, 'error_encode': True})
 
 
 @app.post("/caesar_decode", response_class=HTMLResponse)
 async def caesar_decode(request: Request, text: str = Form(..., description='text'), shift: int = Form(..., description='shift')):
     decoded_text = Caesar.decipher(text=text, shift=shift)
     if decoded_text:
-        return templates.TemplateResponse(request=request, name="caesar.html", context={'input_text_decode': text, 'decoded_text': decoded_text, 'shift_decode': shift, 'error': False})
+        return templates.TemplateResponse(request=request, name="caesar.html", context={'input_text_decode': text, 'decoded_text': decoded_text, 'shift_decode': shift, 'error_decode': False})
     else:
-        return templates.TemplateResponse(request=request, name="caesar.html", context={'input_text_decode': text, 'shift_decode': shift, 'error': True})
+        return templates.TemplateResponse(request=request, name="caesar.html", context={'input_text_decode': text, 'shift_decode': shift, 'error_decode': True})
+
 
 @app.post("/caesar_break_cipher", response_class=HTMLResponse)
 async def caesar_break_cipher(request: Request, text: str = Form(..., description='text')):
     decoded_text = Caesar.break_cipher(text=text)
     if decoded_text:
-        return templates.TemplateResponse(request=request, name="caesar.html", context={'input_text_break_code': text, 'broken_code': decoded_text, 'error': False})
+        return templates.TemplateResponse(request=request, name="caesar.html", context={'input_text_break_code': text, 'broken_code': decoded_text, 'error_break_code': False})
     else:
-        return templates.TemplateResponse(request=request, name="caesar.html", context={'input_text_break_code': text, 'error': True})
+        return templates.TemplateResponse(request=request, name="caesar.html", context={'input_text_break_code': text, 'error_break_code': True})
+
 
 # Vigenere
 
@@ -53,15 +57,27 @@ async def vigenere(request: Request):
 
 
 @app.post("/vigenere_encode", response_class=HTMLResponse)
-async def vigenere_encode(request: Request):
-    return templates.TemplateResponse(request=request, name="caesar.html")
+async def vigenere_encode(request: Request, text: str = Form(..., description='text'), keyword: str = Form(..., description='keyword')):
+    encoded_text = Vigenere.cipher(text=text, keyword=keyword)
+    if encoded_text:
+        return templates.TemplateResponse(request=request, name="vigenere.html", context={'input_text_encode': text, 'encoded_text': encoded_text, 'keyword_encode': keyword, 'error_encode': False})
+    else:
+        return templates.TemplateResponse(request=request, name="vigenere.html", context={'input_text_encode': text, 'keyword_encode': keyword, 'error_encode': True})
 
 
 @app.post("/vigenere_decode", response_class=HTMLResponse)
-async def vigenere_decode(request: Request):
-    return templates.TemplateResponse(request=request, name="caesar.html")
+async def vigenere_decode(request: Request, text: str = Form(..., description='text'), keyword: str = Form(..., description='keyword')):
+    decoded_text = Vigenere.decipher(text=text, keyword=keyword)
+    if decoded_text:
+        return templates.TemplateResponse(request=request, name="vigenere.html", context={'input_text_decode': text, 'decoded_text': decoded_text, 'keyword_decode': keyword, 'error_decode': False})
+    else:
+        return templates.TemplateResponse(request=request, name="vigenere.html", context={'input_text_decode': text, 'keyword_decode': keyword, 'error_decode': True})
 
 
-@app.get("/vigenere_break_cipher", response_class=HTMLResponse)
-async def vigenere_break_cipher(request: Request):
-    return templates.TemplateResponse(request=request, name="caesar.html")
+@app.post("/vigenere_break_cipher", response_class=HTMLResponse)
+async def vigenere_break_cipher(request: Request, text: str = Form(..., description='text')):
+    decoded_text = Vigenere.break_cipher(text=text)
+    if decoded_text:
+        return templates.TemplateResponse(request=request, name="vigenere.html", context={'input_text_break_code': text, 'broken_code': decoded_text, 'error_break_code': False})
+    else:
+        return templates.TemplateResponse(request=request, name="vigenere.html", context={'input_text_break_code': text, 'error_break_code': True})
